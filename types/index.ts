07b898741c727adfc2d85a6b1ce1c58831a00ad7
Manish <manish8726370307@gmail.com>
2025-06-28 15:04:47 +0530

    register user feature added

--- types/index.ts
+++ types/index.ts
@@ -1,31 +1,32 @@
-import { z } from 'zod'
+import { z } from "zod";
 import {
   CartSchema,
   OrderItemSchema,
   ProductInputSchema,
   UserInputSchema,
-  UserSignInSchema
-} from '@/lib/validator'
-export type IProductInput = z.infer<typeof ProductInputSchema>
+  UserSignInSchema,
+  UserSignUpSchema,
+} from "@/lib/validator";
+export type IProductInput = z.infer<typeof ProductInputSchema>;
 export type Data = {
-  products: IProductInput[]
+  products: IProductInput[];
   headerMenus: {
-    name: string
-    href: string
-  }[]
+    name: string;
+    href: string;
+  }[];
   carousels: {
-    image: string
-    url: string
-    title: string
-    buttonCaption: string
-    isPublished: boolean
-  }[]
-   users: IUserInput[]
-  
-}
+    image: string;
+    url: string;
+    title: string;
+    buttonCaption: string;
+    isPublished: boolean;
+  }[];
+  users: IUserInput[];
+};
 
 // user
-export type IUserInput = z.infer<typeof UserInputSchema>
-export type IUserSignIn = z.infer<typeof UserSignInSchema>
-export type OrderItem = z.infer<typeof OrderItemSchema>
-export type Cart = z.infer<typeof CartSchema>
+export type IUserInput = z.infer<typeof UserInputSchema>;
+export type IUserSignIn = z.infer<typeof UserSignInSchema>;
+export type OrderItem = z.infer<typeof OrderItemSchema>;
+export type Cart = z.infer<typeof CartSchema>;
+export type IUserSignUp = z.infer<typeof UserSignUpSchema>;
